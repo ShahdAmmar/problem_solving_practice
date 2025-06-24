2dcf974bc7f50ff669f0af78a0b363a215498977
ShahdAmmar <shahdammar413@gmail.com>
2025-06-24 21:58:00 +0300

    styling

--- easy/second_lowest_score_from_nested_lists.py
+++ easy/second_lowest_score_from_nested_lists.py
@@ -4,6 +4,7 @@ Problem: Given a list of students with their grades, find and print the name(s)
          of the student(s) with the second lowest grade.
 Source: HackerRank
 """
+
 n = int(input("Enter the number of students: "))
 students_data = []
 
@@ -15,7 +16,7 @@ for _ in range(n):
 unique_scores = sorted({s[1] for s in students_data})
 sec_low_score = unique_scores[1]
 
-students_with_sec_low_score = [s[0] for s in students_data if s[1] == sec_low_score]
-students_with_sec_low_score.sort()
-for s in students_with_sec_low_score:
+students_with_sec_low = [s[0] for s in students_data if s[1] == sec_low_score]
+students_with_sec_low.sort()
+for s in students_with_sec_low:
     print(s)
